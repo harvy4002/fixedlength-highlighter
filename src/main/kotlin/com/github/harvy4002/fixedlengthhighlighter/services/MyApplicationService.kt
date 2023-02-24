9bc4112dc2753d415ccae91c9c6ede4e45ce569f
GitHub Action <action@github.com>
2023-02-24 14:57:23 +0000

    Template cleanup

--- src/main/kotlin/com/github/harvy4002/fixedlengthhighlighter/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/harvy4002/fixedlengthhighlighter/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.harvy4002.fixedlengthhighlighter.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.harvy4002.fixedlengthhighlighter.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/harvy4002/fixedlengthhighlighter/services/MyProjectService.kt
+++ src/main/kotlin/com/github/harvy4002/fixedlengthhighlighter/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.harvy4002.fixedlengthhighlighter.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.harvy4002.fixedlengthhighlighter.MyBundle
 
 class MyProjectService(project: Project) {
 
